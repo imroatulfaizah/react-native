4dade2b0d746e30856c9cf754ec2ea950c847c7a
faizah.alfaza <faizah.alfaza@gmail.com>
2020-03-02 03:50:00 -0500

    alert

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
@@ -26,12 +26,19 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos
-      ];
-    })
+    
+    if(text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+    } else {
+      Alert.alert('oops!', 'todos must be over 3 chars long', [
+        {text: 'understood', onPress: () => console.log('alert closed')}
+      ]);
+    }
   }
   
   return (
